abbf5bdf32649491bd2a7614921b1b83b06a06d4
Safae-e <Safae.el.jaouhari@hva.nl>
2024-11-22 10:28:18 +0100

    button aangepast

---
+++ specifity.js
@@ -0,0 +1,6 @@
+let button = document.querySelector('button');
+let navigation = document.querySelector('nav');
+
+button.addEventListener("click", function(){
+  navigation.classList.toggle('show-menu');
+})
