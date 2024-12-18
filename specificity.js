d42b2c65ddcbddaeb4905ab94d8ee946f01ebaca
Safae-e <Safae.el.jaouhari@hva.nl>
2024-12-18 23:29:15 +0100

    story-active toegevoegd

--- specificity.js
+++ specificity.js
@@ -5,3 +5,12 @@ let navigation = document.querySelector('nav');
 button.addEventListener("click", function(){
   navigation.classList.toggle('show-menu');
 })
+
+let div = document.querySelector('.play')
+let playbutton = document.querySelector('playbutton')
+playbutton.addEventListener('click', paused)
+
+function paused() {
+    div.classList.toggle('paused')
+    console.log('doet het')
+  }
